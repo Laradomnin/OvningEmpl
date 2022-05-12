3354c1e022ff9692dbc4c486ed7328315061149d
larad <larad@LAPTOP-19SFN444>
2022-05-12 15:57:11 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖvningEmpl
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public uint Salary { get; set; }
+
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, uint salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Salary: {Salary} kr.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖvningEmpl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var emp1 = new Employee("Lara", 45000);
+            //var emp2 = new Employee();
+            //Employee emp2 = new();
+            //Employee emp3 = new Employee();
+
+            //emp2.Name = "David";
+            //emp2.Salary = 28000;
+
+            //string hej = "hej " + emp2.Name + " " + "Salary: " + ;
+
+            //Console.WriteLine($"Name: {emp1.Name}, Salary: {emp1.Salary} kr.");
+            //Console.WriteLine($"Name: {emp2.Name}, Salary: {emp2.Salary} kr.");
+
+
+            var employees = new List<Employee>();
+
+            Console.WriteLine("How many employees would you like to add?");
+            var nrToAdd = UInt32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nrToAdd; i++)
+            {
+                Console.WriteLine($"Enter name:");
+                var name = Console.ReadLine();
+
+                Console.WriteLine("Enter salary:");
+                var salary = UInt32.Parse(Console.ReadLine());
+
+                var employee = new Employee(name, salary);
+
+                employees.Add(employee);
+
+                Console.WriteLine("\n------------\n");
+            }
+
+            foreach (var e in employees)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+ 
+
+
+            
+
+
+
+            
+
+
+        } 
+        
+
+    }
+}
+
+
+    
+
